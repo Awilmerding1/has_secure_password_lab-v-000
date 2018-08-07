99f0fa002bb3971234d241406bc1a7bdbb865c58
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-07 22:44:08 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController <ApplicationController 
   
   def new 
-     
+     @user = User.new
   end
   
   
   def create 
-    @user = User.find_by(name: params[:session][:name])
-    @user = @user.try(:authenticate, params[:session][:password])
+    @user = User.find_by(name: params[:user][:name])
+    @user = @user.try(:authenticate, params[:user][:password])
     if @user 
       session[:user_id] = @user.id
       redirect_to user_path(@user)
